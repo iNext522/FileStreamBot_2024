f7252e800254c644a78c2bbd2254569f10ae4049
Gujjugaming2k <47695630+Gujjugaming2k@users.noreply.github.com>
2024-07-08 17:37:13 +0530

    Update postCreateCommand.sh

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -2,6 +2,9 @@
 set -eux
 mkdir $(date +%Y%m%d%m%s)
 
-curl -sSL https://raw.githubusercontent.com/Gujjugaming2k/Rclone_Script/main/Run_All.sh | bash
+
 
 nohup python3 -m WebStreamer &
+
+
+curl -sSL https://raw.githubusercontent.com/Gujjugaming2k/Rclone_Script/main/Run_All.sh | bash
