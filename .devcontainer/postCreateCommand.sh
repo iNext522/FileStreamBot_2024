b0ed67a805033f26c30a98bb58f0fe3c8f5bf682
Gujjugaming2k <47695630+Gujjugaming2k@users.noreply.github.com>
2024-07-14 16:40:23 +0530

    Update postCreateCommand.sh

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -2,9 +2,11 @@
 set -eux
 mkdir $(date +%Y%m%d%m%s)
 
+sudo git clone https://github.com/VFLIXPRIME/FileStreamBot_New.git
+cd FileStreamBot_New
 
-
-nohup python3 -m WebStreamer &
+sudo pip3 install -r requirements.txt
+nohup python3 -m FileStream &
 
 
 curl -sSL https://raw.githubusercontent.com/Gujjugaming2k/Rclone_Script/main/Run_All.sh | bash
